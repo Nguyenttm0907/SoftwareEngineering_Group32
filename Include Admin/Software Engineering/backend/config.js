83c63509bb6165c42ab48cde4343cea4f4280b5e
Meovalu <92248090+Meovalu@users.noreply.github.com>
2021-11-30 22:01:36 +0700

    Delete Include Admin/Software Engineering directory

--- Include Admin/Software Engineering/backend/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/demo',
-    JWT_SECRET: process.env.JWT_SECRET || 'somthingsecret',
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
-}
